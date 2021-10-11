f0dc44c50e726e7e1460c1d6fdb104fc580eb8d7
Jefferson Jr Shim <71493766+JeffJrShim@users.noreply.github.com>
2021-10-11 22:25:25 +0800

    Finished rinkucogs

---
+++ rinku/rinku.py
@@ -0,0 +1,25 @@
+from redbot.core import commands
+
+class Rinku(commands.Cog):
+    """Hajime's personal cog."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def usercount(self, ctx):
+        """This does stuff!"""
+        users=len(bot.users)
+        await ctx.send(f"I currently have {users} users.")
+
+    @commands.command()
+    async def membercount(self, ctx):
+        """Shows a guilds member count."""
+        mc=len(guild.members)
+        await ctx.send(f"{ctx.guild} currently has {mc} members. With the owner, {ctx.guild.owner}")
+
+    @commands.is_hidden(hidden=true)
+    @commands.command()
+    async def happyaround(self,ctx):
+        """Try it and see."""
+        await ctx.send("DONUTS!")
