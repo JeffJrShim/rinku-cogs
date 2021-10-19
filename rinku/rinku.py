18df94396d546bcdbe9d333cf8244b23397b9558
Jefferson Jr Shim <71493766+JeffJrShim@users.noreply.github.com>
2021-10-19 18:27:35 +0800

    boba

--- rinku/rinku.py
+++ rinku/rinku.py
@@ -33,7 +33,7 @@ class Rinku(commands.Cog):
         await ctx.send(f"{ctx.guild} currently has {mc} members. With the owner, {ctx.guild.owner}.")
 
     @commands.command()
-    @commands.max_concurrency(1, channel, wait)
+    @commands.max_concurrency(1, Channel, wait)
     async def happyaround(self,ctx):
         """Try it and see."""
         await ctx.send("DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOONUTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTS!")
