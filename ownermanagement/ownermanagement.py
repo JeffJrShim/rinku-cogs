546a9ce8f6d32d6c3829f788b98e4ee507eac0f7
Jefferson Jr Shim <71493766+JeffJrShim@users.noreply.github.com>
2021-10-21 15:19:26 +0800

    Update ownermanagement.py

--- ownermanagement/ownermanagement.py
+++ ownermanagement/ownermanagement.py
@@ -20,7 +20,7 @@ class OwnerManagement(commands.Cog):
         """Add an owner. Be sure to note that adding the user as an owner will give that user access to everything on your bot. Use this command at your own risk."""
         user=self.bot.get_user(user.id)
         self.bot.owner_ids.add(user.id)
-        msg=f"{user} is now a bot owner. Do note that this user currently **has access to everything on the bot, including being able to remove you from ownership** If you've done this by mistake, please do `{ctx.prefix}owner remove {user.id}`"
+        msg=f"{user} is now a bot owner. Do note that this user currently **has access to everything on the bot, including being able to remove your ownership from the bot.** If you've done this by mistake, please do `{ctx.prefix}owner remove {user.id}` Owners set with this command don't persist during restart. To have a more permanent option, use `redbot instancename --owner {ctx.author.id} --co-owner {user.id}`"
         await ctx.send(msg)
 
     @owner.command(invoke_without_command="True")
